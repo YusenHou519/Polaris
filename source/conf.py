b65a90ef56574a0689f4f92052566809f40eb8dc
yusen <houys01@outlook.com>
2025-03-10 19:45:20 +0800

    update

--- source/conf.py
+++ source/conf.py
@@ -9,7 +9,7 @@
 project = 'Polaris'
 copyright = '2024, Yusen Hou, Yanlin Zhang'
 author = 'Yusen Hou, Yanlin Zhang'
-release = '1.0.0'
+release = '1.1.0'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
